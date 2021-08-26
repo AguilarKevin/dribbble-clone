ae745e4ad37e9f77382d96d1b8b9f95d660442e0
AguilarKevin <kevinaguilar0813@gmail.com>
2021-08-26 11:52:18 -0600

    add hover buttons & play video files in shot cards

--- src/components/Home/Cards/Card.jsx
+++ src/components/Home/Cards/Card.jsx
@@ -1,11 +1,30 @@
-import { Image } from '@chakra-ui/react'
+import { Box, Flex, IconButton, Image, Text } from '@chakra-ui/react'
 import React from 'react'
 import CardInfo from './CardInfo'
+import HeartIcon from '../../../assets/heart.svg'
+import SaveIcon from '../../../assets/save-folder.svg'
 
-export default function Card({ media, likes, views, user }) {
+function onMouseOver(e) {
+  e.target.parentElement.querySelector('video').play()
+}
+function onMouseOut(e) {
+  e.target.parentElement.querySelector('video').pause()
+}
+function isVideo(mimetype) {
+  return mimetype === 'video'
+}
+
+export default function Card({ title, media, likes, views, user }) {
   return (
     <div w="100%" h="300px">
-      {mediaview(media)}
+      <Box
+        position="relative"
+        onMouseEnter={isVideo(media[0].mimetype) ? onMouseOver : null}
+        onMouseOut={isVideo(media[0].mimetype) ? onMouseOut : null}
+      >
+        {mediaview(media)}
+        {hoverButtons(title)}
+      </Box>
       <CardInfo avatar={user.avatar} tag={user.tag} username={user.name} />
     </div>
   )
@@ -17,7 +36,6 @@ const mediaview = media => {
       <video
         style={{ height: '230px', borderRadius: '8px', objectFit: 'cover' }}
         muted
-        autoplay
       >
         <source src={`${media[0]?.domain}${media[0]?.path}`} type="video/mp4" />
       </video>
@@ -32,3 +50,37 @@ const mediaview = media => {
     />
   )
 }
+
+const hoverButtons = title => (
+  <Flex
+    position="absolute"
+    align="flex-end"
+    inset="0"
+    zIndex="10"
+    bgGradient="linear(to-t,gray.900, transparent)"
+    px="18px"
+    pb="18px"
+    borderRadius="8px"
+    gridColumnGap="10px"
+    justify="center"
+    opacity="0"
+    transition=" ease 0.2s"
+    _hover={{ opacity: 1 }}
+  >
+    <Text width="60%" color="white" isTruncated>
+      {title}
+    </Text>
+    <IconButton
+      width="34px"
+      height="36px"
+      borderRadius="8px"
+      icon={<Image width="14px" height="14px" src={SaveIcon} />}
+    />
+    <IconButton
+      width="34px"
+      height="36px"
+      borderRadius="8px"
+      icon={<Image width="14px" height="14px" src={HeartIcon} />}
+    />
+  </Flex>
+)
